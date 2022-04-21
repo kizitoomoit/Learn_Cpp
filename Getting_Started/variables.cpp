15b02b1de7f14e545584de2e8ec4f6783771dc14
kizitoomoit <kzitoomoit9@gmail.com>
2022-04-21 19:53:28 +0300

    Next step is using variables in cpp

---
+++ Getting_Started/variables.cpp
@@ -0,0 +1,17 @@
+// This will display the value of the variable.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num;                     // create and integer variable
+
+  num = 63;                    // give a value to the variable
+  cout << "I am ";
+  cout << num;                // displaying the value of num
+  cout << " years old.\n";
+  return 0;
+}
+
+
